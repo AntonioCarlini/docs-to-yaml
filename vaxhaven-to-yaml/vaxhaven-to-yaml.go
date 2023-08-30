b1135f3765846ed7a0af87f3009c6fff0ff41869
Antonio Carlini <antonio@acarlini.com>
2023-08-30 20:12:10 +0100

    vaxhaven-to-yaml.go: Switch to using persistent store
    Remove the initial implementation of the FileSize store and instantiate one based on persistentstore. Remove the now unnecessary code.

--- vaxhaven-to-yaml/vaxhaven-to-yaml.go
+++ vaxhaven-to-yaml/vaxhaven-to-yaml.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"docs-to-yaml/internal/document"
+	"docs-to-yaml/internal/persistentstore"
 	"fmt"
 	"log"
 	"net/http"
@@ -24,27 +25,28 @@ import (
 
 type Document = document.Document
 
-// Md5Cache records information about the MD5 cache itself.
-type FileSizeCache struct {
-	Active                bool             // True if the cache is in use
-	Dirty                 bool             // True if the cache has been modified (and should be written out)
-	CacheOfPathToFileSize map[string]int64 // A cache of path => file size
-}
+type Store = persistentstore.Store[string, int64]
+
+var vaxhaven_prefix = "http://www.vaxhaven.com"
 
 func main() {
 
 	// TODO vaxhaven_yaml := "bin/vaxhaven.yaml"
 	vaxhaven_data := "data/VaxHaven.txt"
 	output_file := "vaxhaven.yaml"
-	fileSizeCacheFilename := "bin/vaxhaven.filesize.cache"
-	fileSizeCacheCreate := true
+	fileSizeStoreFilename := "bin/filesize.store"
+	fileSizeStoreCreate := true
 	verbose := false
-	fileSizeCache, err := FileSizeCacheInit(fileSizeCacheFilename, fileSizeCacheCreate)
+
+	fileSizeStoreInstantiation := persistentstore.Store[string, int64]{}
+	fileSizeStore, err := fileSizeStoreInstantiation.Init(fileSizeStoreFilename, fileSizeStoreCreate, verbose)
 	if err != nil {
-		fmt.Printf("Problem initialising MD5 cache: %+v\n", err)
+		fmt.Printf("Problem initialising FileSize Store: %+v\n", err)
+	} else if !verbose {
+		fmt.Println("Size of new FileSize store: ", len(fileSizeStore.Data))
 	}
 
-	documentsMap := ParseNewData(vaxhaven_data, fileSizeCache, verbose)
+	documentsMap := ParseNewData(vaxhaven_data, fileSizeStore, verbose)
 
 	// Construct the YAML data and write it out to a file
 	data, err := yaml.Marshal(&documentsMap)
@@ -57,18 +59,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// If the File Size cache is active and it has been modified ... write it out
-	if fileSizeCache.Active && fileSizeCache.Dirty {
-		fmt.Println("Writing File Size cache")
-		fileSizeData, err := yaml.Marshal(fileSizeCache.CacheOfPathToFileSize)
-		if err != nil {
-			log.Fatal("Bad File Size data: ", err)
-		}
-		err = os.WriteFile(fileSizeCacheFilename, fileSizeData, 0644)
-		if err != nil {
-			log.Fatal("Failed File Size Cache write: ", err)
-		}
-	}
+	// If the FileSize Store is active and it has been modified ... save it
+	fileSizeStore.Save(fileSizeStoreFilename)
+
 }
 
 // Read the bitsavers IndexByDate.txt file and build a set of paths under DEC-related diectories
@@ -77,7 +70,7 @@ func main() {
 // for example file types such as JPG, BIN and so on are not likely to be
 // worth recording in a list of documents.
 
-func ParseNewData(filename string, fileSizeCache *FileSizeCache, verbose bool) map[string]Document {
+func ParseNewData(filename string, fileSizeStore *Store, verbose bool) map[string]Document {
 
 	// Open the bitsavers index file, complaining loudly on failure
 	file, err := os.ReadFile(filename)
@@ -96,18 +89,13 @@ func ParseNewData(filename string, fileSizeCache *FileSizeCache, verbose bool) m
 
 	documentsMap := make(map[string]Document)
 
-	// r := regexp.MustCompile(`(?ms)<tr>(.*?<td>\s*<a\s+href="(.*?)".*?>(.*?)</a>.*?</td>.*?<td>(.*?)</td>.*?<td>(.*?)</td>.*?)</tr>`)
 	r_rows := regexp.MustCompile(`(?ms)<tr>(.*?)</tr>`)
-	// r_data := regexp.MustCompile(`(?ms)<td>\s*<a\s+href="(.*?)".*?>(.*?)</a>.*?</td>.*?<td>(.*?)</td>.*?<td>(.*?)</td>`)
 	r_data := regexp.MustCompile(`(?ms)<td>\s*<a\s+href="(.*?)".*?>(.*?)</a></td>.*?<td>(.*?)</td>.*?<td>(.*?)</td>`)
 	r_data_no_date := regexp.MustCompile(`(?ms)<td>\s*<a\s+href="(.*?)".*?>(.*?)</a></td>.*?<td>(.*?)</td>`)
 	fmt.Println(r_rows)
 	rows := r_rows.FindAllStringSubmatch(string(file), -1)
-	// fmt.Println(res)
 	fmt.Println("Found ", len(rows), "matches")
 	for i, match := range rows {
-		//fmt.Println("=============================================================================")
-		//fmt.Println("match ", i, " = ", len(match), " => ", match[1])
 		data := r_data.FindAllStringSubmatch(match[1], -1)
 		// data[0][1] => path (href)
 		// data[0][2] => part num
@@ -133,7 +121,8 @@ func ParseNewData(filename string, fileSizeCache *FileSizeCache, verbose bool) m
 				fmt.Printf("Suspicious date [%s] for %s (%s)\n", docDate, document.Title, document.Filepath)
 			}
 		}
-		fileSize, err := CalculatefileSize(document.Filepath, fileSizeCache, verbose)
+
+		fileSize, err := CalculatefileSize(document.Filepath, fileSizeStore, verbose)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -194,61 +183,17 @@ func ConvertVaxHavenDate(date string) string {
 	return result
 }
 
-// Prepares the File Size cache for use.
-// If no MD5 cache file has been specified, create it if allowed.
-// Load YAML data from the cache file (if any).
-//
-// On exit, if no errors occur, the cache is in a valid state.
-func FileSizeCacheInit(fileSizeCacheFilename string, createIfMissing bool) (*FileSizeCache, error) {
-	fileSizeCache := new(FileSizeCache)
-	fileSizeCache.Active = false
-	fileSizeCache.Dirty = false
-	fileSizeCache.CacheOfPathToFileSize = make(map[string]int64)
-	// If a cache exists, read it; possibly create, it if allowed to do so.
-	if fileSizeCacheFilename != "" {
-		file, err := os.ReadFile(fileSizeCacheFilename)
-		if err != nil {
-			if os.IsNotExist(err) {
-				if createIfMissing {
-					newFile, err := os.Create(fileSizeCacheFilename)
-					if err != nil {
-						return fileSizeCache, err
-					}
-					newFile.Close()
-					fmt.Printf("Created empty FileSize cache file: %s\n", fileSizeCacheFilename)
-					file, err = os.ReadFile(fileSizeCacheFilename)
-					if err != nil {
-						return fileSizeCache, err
-					}
-				} else {
-					return fileSizeCache, err
-				}
-			}
-		}
-		fileSizeCache.Active = true
-		// Read the existing cache YAML data into the cache
-		err = yaml.Unmarshal(file, fileSizeCache.CacheOfPathToFileSize)
-		if err != nil {
-			fmt.Println("fileSize cache: failed to unmarshal")
-			return fileSizeCache, err
-		}
-		fmt.Printf("Initial  number of fileSize cache entries: %d\n", len(fileSizeCache.CacheOfPathToFileSize))
-	}
-
-	return fileSizeCache, nil
-}
-
 // Return the fileSize for the specified file.
-// Start by looking up the filename (path) in the cache and return a pre-computed fileSize sum if found.
-// Otherwise, compute the fileSize sum, add the entry to the cache, mark the cache as dirty and return the computed fileSize sum.
+// Start by looking up the filename (path) in the store and return a pre-computed fileSize sum if found.
+// Otherwise, compute the fileSize sum, add the entry to the store and return the computed fileSize sum.
 var tempCount int = 0
 
-func CalculatefileSize(filename string, fileSizeCache *FileSizeCache, verbose bool) (int64, error) {
+func CalculatefileSize(filename string, fileSizeStore *Store, verbose bool) (int64, error) {
 
-	// Lookup the filename (path) in the cache; if found report that as the fileSize sum
-	if fileSize, found := fileSizeCache.CacheOfPathToFileSize[filename]; found {
+	// Lookup the filename (path) in the store; if found report that as the fileSize sum
+	if fileSize, found := fileSizeStore.Lookup(filename); found {
 		if verbose {
-			fmt.Printf("fileSize Cache: Found %d for %s\n", fileSize, filename)
+			fmt.Printf("fileSize Store: Found %d for %s\n", fileSize, filename)
 		}
 		return fileSize, nil
 	}
@@ -258,9 +203,9 @@ func CalculatefileSize(filename string, fileSizeCache *FileSizeCache, verbose bo
 		return 0, nil
 	}
 
-	// The filename (path) is not in the cache.
+	// The filename (path) is not in the store.
 	// Ask for the remote file size
-	url := "http://www.vaxhaven.com" + filename
+	url := vaxhaven_prefix + filename
 	resp, err := http.Head(url)
 	if err != nil {
 		fmt.Println(err)
@@ -268,8 +213,8 @@ func CalculatefileSize(filename string, fileSizeCache *FileSizeCache, verbose bo
 	}
 	time.Sleep(2 * time.Second)
 	fileSize, _ := strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 64)
-	fileSizeCache.CacheOfPathToFileSize[filename] = fileSize
-	fileSizeCache.Dirty = true
-	fmt.Printf("fileSize Cache: wrote %d for %s\n", fileSize, filename)
+	fmt.Printf("fileSize Store: saved %d for %s\n", fileSize, filename)
+	fileSizeStore.Update(filename, fileSize)
+
 	return fileSize, nil
 }
