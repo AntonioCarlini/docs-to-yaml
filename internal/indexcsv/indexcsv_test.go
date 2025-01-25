8dad0927f18a3c352d4bd3493a6108a301b47182
Antonio Carlini <antonio@acarlini.com>
2025-01-25 20:38:03 +0000

    indexcsv_test.go: Add a test for ReadDocumentsFromCsv()
    A simple test to check that a valid CSV file reads properly.

--- internal/indexcsv/indexcsv_test.go
+++ internal/indexcsv/indexcsv_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 )
@@ -20,7 +21,7 @@ func TestWriteDocumentsToCsv(t *testing.T) {
 		"def": {Title: "Doc-2", Md5: "def"},
 	}
 
-	tempFilename := fmt.Sprintf("mytempfile_%s.txt", time.Now().Format("20060102_150405"))
+	tempFilename := HelperCreateTempFileName(0)
 
 	// Call the function to test
 	err := WriteDocumentsToCsv(docsMap, tempFilename)
@@ -68,4 +69,71 @@ func TestWriteDocumentsToCsv(t *testing.T) {
 		t.Fatalf("Error removing file %s: %v", tempFilename, err)
 		return
 	}
+}
+
+func TestReadDocumentsFromCsv(t *testing.T) {
+	// The index and the MD5 checksum must match for the test to work
+	docsMap := map[string]Document{
+		"abc": {Title: "Doc-1", Md5: "abc", Filepath: "doc-1.pdf", Format: "PDF"},
+		"def": {Title: "Doc-2", Md5: "def", Filepath: "doc-2.txt", Format: "TXT"},
+	}
+
+	// Write out a CSV file that matches the data in docsMap
+	// Create the CSV file
+	tempFilename := HelperCreateTempFileName(0)
+	csvFile, err := os.Create(tempFilename)
+
+	if err != nil {
+		t.Fatalf("FATAL: Cannot create CSV file for %s: %v", tempFilename, err)
+	}
+	defer csvFile.Close()
+
+	// Start with the header
+	csvFile.WriteString(fmt.Sprintf("%s\n", strings.Join(CsvHeadings(), ",")))
+	// For each Document, build a
+	for _, doc := range docsMap {
+		// Note: this assumes that doc is simple enough that no special character handling will be needed.
+		csvFile.WriteString(fmt.Sprintf("%s\n", strings.Join(ConvertDocumentToCsv(doc), ",")))
+	}
+	csvFile.Close()
+
+	// Now read back the CSV file and verify the same data comes back
+	loadedDocsMap, err := ReadDocumentsFromCsv(tempFilename)
+	if err != nil {
+		t.Fatalf("FATAL: Cannot load manually created CSV file %s: %v", tempFilename, err)
+	}
+
+	if len(docsMap) != len(loadedDocsMap) {
+		t.Fatalf("FATAL: started with %d docs but loaded %d from CSV", len(docsMap), len(loadedDocsMap))
+	}
+
+	// Walk through original docs map, ensuring that each key exists in the loaded documents and each original oc matches the loaded document
+	for key, _ := range docsMap {
+		if _, present := loadedDocsMap[key]; !present {
+			t.Fatalf("FATAL: original key %s not present in map of loaded docs", key)
+		}
+		if docsMap[key] != loadedDocsMap[key] {
+			t.Fatalf("FATAL: with key %s original doc %v does not match loaded doc %v", key, docsMap[key], loadedDocsMap[key])
+		}
+	}
+
+	// Now perform the same checks but this time starting with loaded docs and checking against the originals
+	for key, _ := range loadedDocsMap {
+		if _, present := docsMap[key]; !present {
+			t.Fatalf("FATAL: loaded key %s not present in map of original docs", key)
+		}
+		if loadedDocsMap[key] != docsMap[key] {
+			t.Fatalf("FATAL: with key %s loaded doc %v does not match original doc %v", key, loadedDocsMap[key], docsMap[key])
+		}
+	}
+
+	// Delete the CSV file that was created
+	err = os.Remove(tempFilename)
+	if err != nil {
+		t.Fatalf("FATAL: cannot delete CSV file %s", tempFilename)
+	}
+}
+
+func HelperCreateTempFileName(index int) string {
+	return fmt.Sprintf("mytempfile_%s_%d.txt", time.Now().Format("20060102_150405"), index)
 }
