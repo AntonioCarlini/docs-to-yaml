ef15b4f70fbbc55e6c307728befa17b034a4e35d
Antonio Carlini <antonio@acarlini.com>
2026-04-28 21:06:42 +0100

    internal/pdfmetadata/pdfmetadata.go: Allow for go-diskfs
    local-archive-to-yaml.go will soon (probably) use go-diskfs to read ISO volumes. In preparation for that some of the internal tools need to allow for the possibility that they may be fed an fs.FS. This is the first such tool that needs that. Unfortunately go-exiftool can't hadle fs.FS (afaik) so the somewhat ugly solution is to write a temporary file. That will do for now.

--- internal/pdfmetadata/pdfmetadata.go
+++ internal/pdfmetadata/pdfmetadata.go
@@ -2,7 +2,9 @@ package pdfmetadata
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/barasher/go-exiftool"
@@ -18,6 +20,17 @@ type PdfMetadata struct {
 
 // Given a PDF file, this function finds the associated metdata and returns those elements that will be stored in the YAML.
 func ExtractPdfMetadata(pdfFilename string) PdfMetadata {
+	et, err := exiftool.NewExiftool()
+	if err != nil {
+		log.Printf("Error when initializing: %v\n", err)
+	}
+	defer et.Close()
+
+	fileInfos := et.ExtractMetadata(pdfFilename)
+	return parseExifOutput(fileInfos) // Use the new helper logic
+}
+
+func ZExtractPdfMetadata(pdfFilename string) PdfMetadata {
 	et, err := exiftool.NewExiftool()
 	if err != nil {
 		log.Printf("Error when intializing: %v\n", err)
@@ -48,5 +61,64 @@ func ExtractPdfMetadata(pdfFilename string) PdfMetadata {
 		}
 	}
 
+	return metadata
+}
+
+func ExtractPdfMetadataFromFS(fsys fs.FS, pdfPath string) PdfMetadata {
+	// 1. Read data from the ISO into memory
+	data, err := fs.ReadFile(fsys, pdfPath)
+	if err != nil {
+		log.Printf("Error reading from FS: %v\n", err)
+		return PdfMetadata{}
+	}
+
+	// 2. Create a temporary file on the local disk
+	tmpFile, err := os.CreateTemp("", "exiftool-*.pdf")
+	if err != nil {
+		log.Printf("Error creating temp file: %v\n", err)
+		return PdfMetadata{}
+	}
+	defer os.Remove(tmpFile.Name()) // Clean up after we are done
+
+	// 3. Write the bytes to the temp file
+	if _, err := tmpFile.Write(data); err != nil {
+		tmpFile.Close()
+		return PdfMetadata{}
+	}
+	tmpFile.Close() // Close so Exiftool can access it
+
+	// 4. Use the standard ExtractMetadata call on the temp path
+	et, err := exiftool.NewExiftool()
+	if err != nil {
+		log.Printf("Error when initializing: %v\n", err)
+	}
+	defer et.Close()
+
+	fileInfos := et.ExtractMetadata(tmpFile.Name())
+	return parseExifOutput(fileInfos)
+}
+
+// Private helper to avoid repetition
+func parseExifOutput(fileInfos []exiftool.FileMetadata) PdfMetadata {
+	metadata := PdfMetadata{}
+	for _, fileInfo := range fileInfos {
+		if fileInfo.Err != nil {
+			fmt.Printf("Error concerning %v: %v\n", fileInfo.File, fileInfo.Err)
+			continue
+		}
+
+		for k, v := range fileInfo.Fields {
+			switch k {
+			case "Creator":
+				metadata.Creator = v.(string)
+			case "Producer":
+				metadata.Producer = v.(string)
+			case "PDFVersion":
+				metadata.Format = strings.TrimRight(fmt.Sprintf("%f", v.(float64)), "0")
+			case "ModifyDate":
+				metadata.Modified = v.(string)
+			}
+		}
+	}
 	return metadata
 }
